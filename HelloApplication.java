c1ed771406b9b79d9898fd9b7f46060bb3b11ff1
Suryateja Challa <suryatejachalla.dev@gmail.com>
2023-12-16 09:47:49 +0530

    Update Application.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -10,10 +10,10 @@ import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Media Player");
         stage.setScene(scene);
